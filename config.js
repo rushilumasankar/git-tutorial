8795f82e3fda9aa7e3aa8b9cac54c6939c2a2d4f
rushil <zenflickerenola@gmail.com>
2025-03-27 23:10:05 +0530

    version 1

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    appName: "GitHub Practice App",
+    version: "1.0.0",
+    author: "Your Name",
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,5 @@
+const config = require("./config");
+
+console.log(`App Name: ${config.appName}`);
+console.log(`Version: ${config.version}`);
+console.log(`Author: ${config.author}`);
